1e8768717a558f5d38d718e2f158fd4e540789da
Muthuram <rmtmuthuram@gmail.com>
2025-11-05 00:29:45 +0530

    added in main go

--- controller/controllers.go
+++ controller/controllers.go
@@ -1,1 +1,26 @@
 package controller
+
+import (
+	"fmt"
+
+	"github.com/21RMT14Muthuram/my-new-app/model"
+)
+
+var users []models.User
+
+
+
+func NewPrint(){
+	fmt.Println("this new one !!!")
+}
+
+func AddSingleUser() {
+    user := models.User{
+        Username: "Muthuram",
+        Password: "12345",
+    }
+
+    users = append(users, user)
+
+    fmt.Println("Added user:", user)
+}
--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+
+	"github.com/21RMT14Muthuram/my-new-app/controller"
+	"github.com/gin-gonic/gin"
+)
 
 func main(){
-	fmt.Print("Hello, World!")
+	// fmt.Print("Hello, World!")
+	// controller.NewPrint()
+	// controller.AddSingleUser()
+	r := gin.Default()
+
+	//public routes
+	r.POST("./signup", controller.SignUpHandler)
+	r.POST("/login", controller.LoginHandler)
+
+
+	r.Run(":8000")
 }
